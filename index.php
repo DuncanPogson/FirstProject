af37c29aa749cb1fb0a3d45e4ac96f156a7a567f
duncanpogson <duncan_pogo@hotmail.com>
2016-10-03 15:10:05 +0100

    First PHP Index

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        </head>
+        <body>
+            <p>
+                <?php 
+                    echo "hello World";
+                ?>
+            </p>
+        </body>
+
+</html>
+
+
+/**
+ * Created by PhpStorm.
+ * User: 1405466
+ * Date: 03/10/2016
+ * Time: 15:06
+ */

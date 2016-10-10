be7807d84f4bc0e421237561261bfc3363733147
duncanpogson <duncan_pogo@hotmail.com>
2016-10-10 14:35:46 +0100

    Adding Adminer File

---
+++ Week3/DBCONNECT.php
@@ -0,0 +1,15 @@
+<?php
+
+define('DB_SERVER', 'us-cdbr-azure-southcentral-f.cloudapp.net');
+define('DB_USERNAME', 'b2ccbc0609a4ce');
+define('DB_PASSWORD', '24c190f1');
+define('DB_DATABASE', 'dp_database');
+
+$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+/**
+ * Created by PhpStorm.
+ * User: 1405466
+ * Date: 10/10/2016
+ * Time: 14:18
+ */

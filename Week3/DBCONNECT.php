cd9c681ab228894294021fe9e9b92fec76902830
duncanpogson <duncan_pogo@hotmail.com>
2016-10-10 14:36:26 +0100

    Adding Adminer File

--- Week3/DBCONNECT.php
+++ Week3/DBCONNECT.php
@@ -6,9 +6,8 @@ define('DB_PASSWORD', '24c190f1');
 define('DB_DATABASE', 'dp_database');
 
 $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
-
 /**
- * Created by PhpStorm.
+ * Created by Duncan.
  * User: 1405466
  * Date: 10/10/2016
  * Time: 14:18
